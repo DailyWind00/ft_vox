3277ff53df7916103ac900db01fa4cfce5aab82a
DailyWind00 <gallaismael0@gmail.com>
2025-01-10 18:30:51 +0100

    geometry shader tests, to removes

--- srcs/rendering.cpp
+++ srcs/rendering.cpp
@@ -1,37 +1,59 @@
 #include "config.hpp"
 
-// Temporary function to test the profiler (to remove)
-static void	countTo(int num)
-{
-	int	i = 0;
-
-	while (i < num)
-		i++;
-}
-
 // Keep the window alive, exiting this function should mean closing the window
-static void program_loop(Window &window, Profiler &pr) {
-	while (!glfwWindowShouldClose(window)) {
-		glfwSwapBuffers(window);
+static void program_loop(Window &window, Profiler &pr, Shader &shader) {
+	// To remove
+	glm::vec3	positions[] = {
+		glm::vec3(0.0f, 0.0f, 0.0f),
+	};
 
-		// temporary (to remove)
-		pr.evaluateNoReturn("countTo100000000", &countTo, 100000000);
-		pr.evaluateNoReturn("countTo10000000", &countTo, 10000000);
-		pr.evaluateNoReturn("countTo1000000", &countTo, 1000000);
-		pr.evaluateNoReturn("countTo100000", &countTo, 100000);
-		pr.evaluateNoReturn("countTo10000", &countTo, 10000);
-		pr.evaluateNoReturn("countTo1000", &countTo, 1000);
-		// --
+	GLuint VAO, VBO;	
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+	glEnableVertexAttribArray(0);
+	// --
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		// Update
+		glm::mat4 model      = glm::mat4(1.0f); // Identity matrix
+		glm::mat4 view       = glm::lookAt(glm::vec3(0, 0, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+		glm::mat4 mvp        = projection * view * model;
+		shader.setUniform("transform", mvp);
+
+		// Draw call
+		glDrawArrays(GL_POINTS, 0, 1);
 
 		handleEvents(window);
+		glfwSwapBuffers(window);
 	}
+
+	(void)pr;
+	(void)shader;
 }
 
 // Setup variables and call the program loop
 void	Rendering(Window &window) {
-	Profiler	pr;
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	program_loop(window, pr);
+	Profiler	pr;
+	Shader		shader("shaders/vertex.glsl", "shaders/fragment.glsl", "shaders/geometry.glsl");
+	shader.use();
+
+	printVerbose("Entering program's loop");
+	program_loop(window, pr, shader);
+	printVerbose("Exiting program's loop");
 
 	pr.logToFile("out");
 }

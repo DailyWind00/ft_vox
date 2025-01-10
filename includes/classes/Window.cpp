86537ce6b7be3f4d2a810687d04fda6e8612f3fb
DailyWind00 <gallaismael0@gmail.com>
2025-01-10 18:29:51 +0100

    Shader correctly link geometry shader & square viewport

--- includes/classes/Window.cpp
+++ includes/classes/Window.cpp
@@ -33,7 +33,9 @@ Window::Window(int posX, int posY, int width, int height, const std::string &tit
 		std::cout << "> GLAD initialized" << std::endl;
 
 	glfwSetWindowPos(window, posX, posY);
-	glViewport(0, 0, width, height);
+
+	int viewportSize = std::min(width, height); // Square viewport
+	glViewport((width - viewportSize) / 2, (height - viewportSize) / 2, viewportSize, viewportSize);
 
 	if (VERBOSE)
 		std::cout << "Window created" << std::endl;

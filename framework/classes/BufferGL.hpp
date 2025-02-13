e38a4cfeb672ccbc70c75acca9f7fc1f06b8c470
DailyWind00 <gallaismael0@gmail.com>
2025-02-13 08:35:00 +0100

    Added BufferGL class

---
+++ framework/classes/BufferGL.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+/// Defines
+# define COLOR_HEADER_CXX
+
+/// System includes
+# include <iostream>
+# include <vector>
+
+/// Dependencies
+# include <glad/glad.h>
+# include "color.h"
+
+/// Global variables
+extern bool VERBOSE;
+
+// This class is a simple wrapper around OpenGL buffers
+// It allows to create, bind, unbind, update and delete buffers
+class BufferGL {
+	private:
+		GLuint	_id;
+		GLenum	_type;
+		GLenum	_usage;
+		size_t	_capacity;
+
+	public:
+		BufferGL();
+		BufferGL(GLenum type, GLenum usage, size_t size = 0, const void *data = nullptr);
+		~BufferGL();
+
+		/// Public functions
+
+		void	bind();
+		void	unbind();
+		void	updateData(const void *data, size_t size, size_t offset);
+		void	resize(size_t newSize, const void *data = nullptr);
+		void	clear();
+
+		/// Getters
+
+		const GLuint	&getID() const;
+		const GLenum	&getType() const;
+		const size_t	&getCapacity() const;
+		bool			isValid() const;
+};
